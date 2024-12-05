940d9545c2cf0e179b6031b0f0a8a9fcb43f2129
Dawid64 <dawid.siera@gmail.com>
2024-12-05 20:13:42 +0100

    added noise to data generator

--- face_recognition/model/model.py
+++ face_recognition/model/model.py
@@ -5,6 +5,22 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from face_recognition.utils import draw_bounding_box
+import random
+
+def random_transform(image, x, y, width, height, original_width, original_height):
+    if random.choice([True, False]):
+        image = cv2.flip(image, 1)
+        x = original_width - x - width 
+
+    angle = random.uniform(-15, 15)
+    matrix = cv2.getRotationMatrix2D((original_width / 2, original_height / 2), angle, 1)
+    image = cv2.warpAffine(image, matrix, (original_width, original_height))
+
+    if random.choice([True, False]):
+        ksize = random.choice([3, 5])
+        image = cv2.GaussianBlur(image, (ksize, ksize), 0)
+
+    return image, x, y, width, height
 
 def data_generator(csv_path, batch_size=32, image_size=(128, 128)):
     data = pd.read_csv(csv_path)
@@ -19,13 +35,22 @@ def data_generator(csv_path, batch_size=32, image_size=(128, 128)):
                 if image is None:
                     continue
                 original_height, original_width = image.shape[:2]
+
+                x = float(row['x'])
+                y = float(row['y'])
+                width = float(row['w'])
+                height = float(row['h'])
+                image, x, y, width, height = random_transform(
+                    image, x, y, width, height, original_width, original_height
+                )
                 image = cv2.resize(image, image_size) / 255.0
-                x = float(row['x']) / original_width
-                y = float(row['y']) / original_height
-                width = float(row['w']) / original_width
-                height = float(row['h']) / original_height
+                normalized_x = x / original_width
+                normalized_y = y / original_height
+                normalized_width = width / original_width
+                normalized_height = height / original_height
+
                 images.append(image)
-                labels.append([x, y, width, height])
+                labels.append([normalized_x, normalized_y, normalized_width, normalized_height])
             yield np.array(images, dtype=np.float32), np.array(labels, dtype=np.float32)
 
 
